7ecd48688c882d49e791eca42da05a99138ed0b4
kaafivikrant <iamvikrant1@gmail.com>
2023-08-14 13:59:55 +0530

    dd

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Welcome to Deno by Vikrant !");
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+function handler(_req) {
+  // Create a response with html as its body.
+  const response = new Response("<html> Hello Vikrant </html>", {
+    status: 200,
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+
+  console.log(response.status); // 200
+  console.log(response.headers.get("content-type")); // text/html
+
+  return response;
+}
+
+serve(handler);
