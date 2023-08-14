0e68edf9c3990e0c5f22a7ef2da6d9abea2ecf2c
kaafivikrant <iamvikrant1@gmail.com>
2023-08-14 21:42:04 +0530

    dc

--- index.ts
+++ index.ts
@@ -1,18 +1,38 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-function handler(_req) {
-  // Create a response with html as its body.
-  const response = new Response("<html> Hello Vikrant </html>", {
-    status: 200,
+async function handler(req: Request): Promise<Response> {
+    const resp = await fetch("https://api.wintwealth.com/products/all?userId=125811", {
+      // The init object here has an headers object containing a
+      // header that indicates what type of response we accept.
+      // We're not specifying the method field since by default
+      // fetch makes a GET request.
     headers: {
-      "content-type": "text/html",
+        "authority": "api.wintwealth.com",
+        "accept": "*/*",
+        "accept-language": "en-GB,en;q=0.9",
+        "content-type": "application/json",
+        "origin": "https://www.wintwealth.com",
+        "referer": "https://www.wintwealth.com/",
+        "sec-ch-ua": '"Not/A)Brand";v="99", "Brave";v="115", "Chromium";v="115"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "sec-gpc": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome",
+        "x-auth-token": "qDV2aGzfzAiRseHMbrNoGg"
     },
-  });
-
-  console.log(response.status); // 200
-  console.log(response.headers.get("content-type")); // text/html
-
-  return response;
+    });
+    // Put the response in the variable
+    // let data = await resp.json();
+    // console.log(data);
+    return new Response(resp.body, {
+        status: resp.status,
+        headers: {
+        "content-type": "application/json",
+        },
+    });
 }
 
 serve(handler);
